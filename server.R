8b14b086266edae02b6072e8f26dbe623e6b12de
John Joyce <Joyce_John@student.ceu.edu>
2021-06-08 23:19:10 +0200

    serverside selectize, nicer city and country name strings

--- server.R
+++ server.R
@@ -11,47 +11,67 @@ server <- function(input, output, session) {
   
   ### -----> TODAY'S WEATHER OUTPUT <----- ###
   
-  output$curr_temp <- renderInfoBox({infoBox(title = "Temperature", value = paste0(get_current_weather(location = input$curr_city, unit_type = input$curr_measurement)$temperature_current, "°"), icon = icon('thermometer-half'))
-  })
   
-  output$curr_feel <- renderInfoBox({infoBox(title = "Feels like", value = paste0(round(get_current_weather(location = input$curr_city, unit_type = input$curr_measurement)$temperature_feels_like, 0), "°"), icon = icon('hand-holding'))
-  })
+  updateSelectizeInput(session, 'curr_city', choices = cities$unique_code, selected = "Budapest, HU", server = TRUE)
   
-  output$curr_condition <- renderInfoBox({infoBox(title = "Condition", value = get_current_weather(location = input$curr_city, unit_type = input$curr_measurement)$specific_description, icon = icon('globe'))
-  })
-  
-  output$curr_image <- renderImage({
-    list(
-      src = paste0("www/",get_current_weather(location = input$curr_city)$general_description,".jpg"),
-      contentType = "image/jpg",
-      height = 800, # session$clientData$output_myImage_height, #might work with tweaks
-      width =  2000, # session$clientData$output_myImage_width, # might work with tweaks
-      alt = "scientific_photo_of_weather")
+  observeEvent(c(input$curr_city,input$curr_measurement), {
     
+    curr_weather <- get_current_weather(location = input$curr_city, unit_type = input$curr_measurement)
     
-  }, deleteFile = FALSE)
+    output$curr_temp <- renderInfoBox({infoBox(title = "Temperature", value = paste0(round(curr_weather$temperature_current,0), "°"), icon = icon('thermometer-half'))
+    })
+    
+    output$curr_feel <- renderInfoBox({infoBox(title = "Feels like", value = paste0(round(curr_weather$temperature_feels_like, 0), "°"), icon = icon('hand-holding'))
+    })
+    
+    output$curr_condition <- renderInfoBox({infoBox(title = "Condition", value = curr_weather$specific_description, icon = icon('globe'))
+    })
+    
+    output$curr_image <- renderImage({
+      list(
+        src = paste0("www/",curr_weather$general_description,".jpg"),
+        contentType = "image/jpg",
+        height = 800, # session$clientData$output_myImage_height, #might work with tweaks
+        width =  2000, # session$clientData$output_myImage_width, # might work with tweaks
+        alt = "scientific_photo_of_weather")
+      
+      
+    }, deleteFile = FALSE)
+    
+  })
   
 
 
   ### -----> FORECAST WEATHER OUTPUT <----- ###
   
-  output$plot_forecast <- renderGirafe({
-    girafe(ggobj = make_weekly_forecast_plot(input_forecast_city = input$forecast_city, input_forecast_measurement = input$forecast_measurement))
+  updateSelectizeInput(session, 'forecast_city', choices = cities$unique_code, selected = "Budapest, HU", server = TRUE)
+  
+  observeEvent(c(input$forecast_city,input$forecast_measurement), {
+    
+    # get weather forecast for selected city. if there is more than one city with the selected name, take the one with the largest population
+    forecast_weather <-  get_forecast(longitude = cities[cities$unique_code == input$forecast_city][order(-population)][1]$lng,
+                                      latitude = cities[cities$unique_code == input$forecast_city][order(-population)][1]$lat,
+                                      unit_type = input$forecast_measurement)
+    
+    output$plot_forecast <- renderGirafe({
+      girafe(ggobj = make_weekly_forecast_plot(forecast_weather))
+    })
+    
+    # do some data.table column name manipulations for a nicer table
+    output$forecast_table <- renderDataTable({
+      
+      forecast_weather[,  .(Date = date, 
+                            Day = day_of_week, 
+                            Condition = specific_description,
+                            Temp = temp_daytime,
+                            High = temp_max,
+                            Low = temp_min)]
+      
+    }, options = list(dom = 't')) # options to hide unneeded UI elements
+    
+    
   })
-  
-  
-  output$forecast_table <- renderDataTable({
-    get_forecast(longitude = cities$lng[cities$unique_code == input$forecast_city],
-                 latitude = cities$lat[cities$unique_code == input$forecast_city],
-                 unit_type = input$forecast_measurement)[,  .(Date = date, 
-                                                              Day = day_of_week, 
-                                                              Condition = specific_description,
-                                                              Temp = temp_daytime,
-                                                              High = temp_max,
-                                                              Low = temp_min)]
-  }, options = list(dom = 't'))
-  
-  
+
   
   ### -----> APP SURVEY PAGE <----- ###
   
